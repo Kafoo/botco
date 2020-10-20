c5f920f958eefb1f2c5cbbb2ac4cce176ff8992c
Unknown <ant.guillard@gmail.com>
2020-10-20 21:39:35 +0200

    minupdate

--- index.js
+++ index.js
@@ -85,17 +85,18 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+
   let response;
 
   // Check if the message contains text
   if (received_message.text) {    
 
     /*response = bot.answer(received_message.text)*/
-    response = {'text': 'salut !!'}
+    response = bot.answer(received_message.text)
 
     if (response) {
-
-    callSendAPI(sender_psid, response);  
+      response = {'text': response}
+      callSendAPI(sender_psid, response);  
     }
 
   }  
